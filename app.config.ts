80f38ae620cc63fddcbaa50362b41e37a78db8f6
root <root@mobileapp.us-central1-c.c.mobileapp-480902.internal>
2025-12-15 13:21:39 +0000

    Fix: Make google-services.json optional using environment variable

--- app.config.ts
+++ app.config.ts
@@ -40,7 +40,9 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       package: 'com.letthemconnect.app',
       permissions: ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'],
       // Please use the relative path to the google-services.json file
-      googleServicesFile: "./google-services.json",
+      ...(process.env.EXPO_PUBLIC_ANDROID_GOOGLE_SERVICES_FILE && {
+        googleServicesFile: process.env.EXPO_PUBLIC_ANDROID_GOOGLE_SERVICES_FILE,
+      }),
       intentFilters: [
         {
           action: 'VIEW',
